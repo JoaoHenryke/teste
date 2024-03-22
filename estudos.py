66343d1ab1909f425ed44efb77efb67b9231ad87
João_Henrique <joaoh9121@outlook.com>
2024-03-22 15:38:01 -0300

    arquivo novo

---
+++ estudos.py
@@ -0,0 +1,109 @@
+# curso de python (udemy) 
+#operadores in(entre/estar entre) e not in(não entre/ não esta entre)
+#operadores são iteráveis(podem ser navagados separadamente, em qualquer string usando seus respctivos indices, lembrnado que o indice começa por "0", tendo como indice positivos com negativos.)
+
+# 0  1  2  3  4  5  6  7 - indices positivos
+
+# H  e  m  r  i  q  u  e - string
+
+#-8 -7 -6 -5 -4 -3 -2 -1- indices negativos / no caso do indece negativo começa a contar do -1, pois o "0" já começa a ontagem de indece positivos.abs
+
+
+#nome = "Henrique"
+#print(nome[2], nome [-5]) #para navagar na string nome, na funcão print, noma da variavél onde armazena a string que deseja navagr + usa-se a chava [ ] para selecionar o indece.
+"""
+nome = "Henrique"
+
+print("Hen" in nome)
+print("que" in nome)
+print(10 * "-")
+print("Hen" not in nome)
+print("Hen" not in nome)
+
+#  Brincando com os operadores
+
+nome  = input("Digite seu nome: ")
+encontra = input("Digite o que deseja encontrar: ")
+
+if encontra in nome:
+    print(f"{encontra} está em {nome}")
+else:
+    print(f"{encontra} não está em {nome}")
+    
+
+    varaia_a = 1 or 0
+    varaia_b = 0 or 1
+
+    print(varaia_a, varaia_b)
+
+
+nome = "Maria carmo"
+
+if " " in nome:
+    print(f"O nome {nome} tem espaço")
+else:
+    print(f"O nome{nome} tem esepaço")
+
+numero = 10
+
+if numero > 1:
+    if numero > 2:
+        if numero > 3:
+            print("Número maior que 3")
+        else:
+            print("Número maior que 3")
+    else:
+        print("Número menor que 2")
+else:
+    print("Número menor que 1")
+
+
+
+nome = input("Digite seu nome: ")
+palavra = input("Digite uma letra que deseja encontra: ")
+
+if palavra in nome:
+    print(f"a letras {palavra} está contida no seu nome")
+else:
+    print(f"a letra {palavra} não foi encontrada")
+
+"""
+"""
+interpolação básica de strings
+
+s  - string
+d e i - int
+f - float
+X e x,
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ - Hexadecimal (ABCDEF0123456789)
+
+"""
+
